107335be241c0316fb6aa487e73af1876e5c2fd9
Vickyprasad2000 <125580199+Vickyprasad2000@users.noreply.github.com>
2024-09-20 13:08:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function fibonacci(num) {
 // your code here
+	let num1 = 0;
+	let num2 = 1;
+	let sum;
+
+	if(num == 0){
+		return num1;
+	}else if(num == 1){
+		return num2;
+	}else {
+		for(let i=0;i<=num;i++){
+			sum = num1 + num2;
+			num1 = num2;
+			num2 = sum;
+		}
+		return sum;
+	}
 }
 
 module.exports = fibonacci;
